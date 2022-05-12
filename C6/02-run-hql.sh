16e55066bf6d032f73db9468ccbd3356f484116e
Manoj Shanmugasundaram <manoj.sundaram21@gmail.com>
2022-05-12 22:11:24 +0000

    adding dropp all db script

--- C6/02-run-hql.sh
+++ C6/02-run-hql.sh
@@ -1,12 +1,18 @@
  #!/bin/bash
 
 export WKDIR=/tmp
-export beeline_url="jdbc:hive2://localhost:10000"
+export kdc_realm=SQUADRON.SUPPORT.HORTONWORKS.COM
+export beeline_url="jdbc:hive2://localhost:10000/default;principal=hive/$(hostname -f)@${kdc_realm}"
 
+kinit -kt /run/cloudera-scm-agent/process/93-hive-HIVEMETASTORE/hive.keytab hive/`hostname -f`@$kdc_realm
 cd $WKDIR
+rm -rf $WKDIR/CDP-SaaS-masterclass
 
-beeline -u ${beeline_url} -n hive -f $WKDIR/data/HiveSchema.hsql
+git clone https://github.com/manojsundaram/CDP-SaaS-masterclass
+
+beeline -u ${beeline_url} -f $WKDIR/CDP-SaaS-masterclass/data/dropall_db.hsql
+beeline -u ${beeline_url} -f $WKDIR/CDP-SaaS-masterclass/data/HiveSchema.hsql
 
 if [ "${enable_hive_acid}" = true  ]; then
-  beeline -u ${beeline_url} -n hive -f $WKDIRdata/TransSchema.hsql
+  beeline -u ${beeline_url} -n hive -f $WKDIR/CDP-SaaS-masterclass/data/TransSchema.hsql
 fi
